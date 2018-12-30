0f13f1e73629a2e61c0c20fb9d8e0225ac2dae2b
Felipe Dias <felipelimadiaz@yahoo.com.br>
2018-12-30 02:52:36 -0200

    GUI build

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -15,15 +15,18 @@ public class Controller implements Observer {
     private Repo repo;
     @FXML
     private VBox vBox;
-
     @FXML
     private VBox vBox2;
+    @FXML
+    private AnchorPane pane;
 
     public void initialize() throws Exception {
         RepoFactory repoFactory = new RepoFactoryImpl();
         this.repo = repoFactory.getRepo();
         repo.addObserver(this);
         this.vBox.setStyle("-fx-border-color: black;" + "-fx-border-style: solid;" + "-fx-border-width: 1");
+        this.vBox.prefWidthProperty().bind(this.pane.widthProperty().multiply(1));
+        this.vBox.prefHeightProperty().bind(this.pane.heightProperty().multiply(1));
 
         for (Build build : repo.getBuilds()) {
             Label label = new Label(build.getState());
@@ -36,10 +39,13 @@ public class Controller implements Observer {
     public void update() throws Exception {
         List<Build> builds = repo.getBuilds();
         this.vBox2.getChildren().clear();
+        this.vBox2.prefWidthProperty().bind(this.vBox.widthProperty().multiply(1));
+        this.vBox2.prefHeightProperty().bind(this.vBox.heightProperty().multiply(1));
 
         for (Build build : builds){
             Label label = new Label(build.getState());
-            label.setMaxWidth(250);
+            label.prefWidthProperty().bind(this.vBox2.widthProperty().multiply(1));
+            label.prefHeightProperty().bind(this.vBox2.heightProperty().multiply(1));
             label.setStyle("-fx-border-color: black;" + "-fx-border-style: solid none none solid;" + "-fx-border-width: 1");
             this.vBox2.getChildren().add(label);
         }
