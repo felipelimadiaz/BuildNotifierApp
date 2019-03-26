51a4d298b6f86609e4650fa17df3114cf1ca2877
Felipe Dias <felipelimadiaz@yahoo.com.br>
2019-03-26 00:48:07 -0300

    changed gson version to run jcloud

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,7 +1,10 @@
 package sample;
+
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) throws Exception {
         JavaFxUI.launch(args);
+        }
     }
-}
